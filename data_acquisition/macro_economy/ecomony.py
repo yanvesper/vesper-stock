661f161a8145587df7c8b0570534cdf140a7214f
yanvesper <yanvesper@gmail.com>
2019-04-21 14:41:01 +0800

    restructure the project

---
+++ data_acquisition/macro_economy/ecomony.py
@@ -0,0 +1,5 @@
+from data_acquisition.macro_economy import shibor
+
+
+def economy_acquisition(pro_interface):
+    shibor.shibor_acquisition(pro_interface)
--- data_acquisition/main.py
+++ data_acquisition/main.py
@@ -1,9 +1,9 @@
 import tushare as ts
-from data_acquisition import shibor
+from data_acquisition.macro_economy import ecomony
 
 # 设置token
 ts.set_token('0f99a58cc679fa01b44daa1e2219d032b9c7830e79a12f02acb3edab')
 
 # 初始化pro接口
 pro = ts.pro_api()
-shibor.shibor_acquisition(pro_interface=pro)
+ecomony.economy_acquisition(pro_interface=pro)
